91ab38c7d2261c1900f6d9b5faedb80741d32bb5
AkshayDholakiya <96297018+akshayd021@users.noreply.github.com>
2025-05-30 15:03:02 +0530

    change

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://employee-frontend-i28v.onrender.com/",
+    origin: "https://employee-frontend-i28v.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
